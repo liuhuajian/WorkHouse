e1d1893f3e56f7c5422ba36ec6caea74c0f84bba
liuhuajian <liuhuajian_lhj@163.com>
2018-11-20 00:33:13 +0800

    新增paylib、imagePicker模块

--- app/src/main/java/messi/lhj/com/modultest/MainActivity.java
+++ app/src/main/java/messi/lhj/com/modultest/MainActivity.java
@@ -2,6 +2,7 @@ package messi.lhj.com.modultest;
 
 import android.os.Bundle;
 
+import com.julyzeng.imagepicker.ui.ImageGridActivity;
 import com.project.common.core.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -9,6 +10,11 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        test();
+    }
+
+    private void test() {
+        ImageGridActivity imageGridActivity = new ImageGridActivity();
     }
 
     @Override
