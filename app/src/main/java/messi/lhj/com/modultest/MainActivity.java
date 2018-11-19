3f432a82449880230f3f9040e70e8b044f4fa8a6
liuhuajian <liuhuajian_lhj@163.com>
2018-11-20 00:16:34 +0800

    去除测试模块

--- app/src/main/java/messi/lhj/com/modultest/MainActivity.java
+++ app/src/main/java/messi/lhj/com/modultest/MainActivity.java
@@ -2,9 +2,6 @@ package messi.lhj.com.modultest;
 
 import android.os.Bundle;
 
-import com.clj.fastble.BleManager;
-import com.julyzeng.baserecycleradapterlib.BaseRecyclerAdapter;
-import com.julyzeng.baserecycleradapterlib.BaseViewHolder;
 import com.project.common.core.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -12,7 +9,6 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        test();
     }
 
     @Override
@@ -20,13 +16,4 @@ public class MainActivity extends BaseActivity {
         return R.layout.activity_main;
     }
 
-    private void test() {
-        BaseRecyclerAdapter<String> baseRecyclerAdapter = new BaseRecyclerAdapter<String>(mContext, null, R.layout.item_test) {
-            @Override
-            protected void convert(BaseViewHolder helper, String item) {
-
-            }
-        };
-        BleManager.getInstance().init(getApplication());
-    }
 }
